8b785f81b878f060c20b43114008568141c70260
Joeydm6 <joey@klassebv.nl>
2024-12-14 10:14:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const generateQuestionsWithWeights = (formId, questions) => {
         const form = document.getElementById(formId);
-        form.innerHTML = ""; // Clear form
+        form.innerHTML = "";
 
         questions.forEach(({ question, type, name, options }) => {
             const container = document.createElement("div");
@@ -187,7 +187,7 @@ document.addEventListener("DOMContentLoaded", () => {
         promptText.textContent = prompt || "Your prompt will appear here after you make a selection.";
         examplePromptDiv.classList.remove("d-none");
         examplePromptDiv.style.display = "block";
-        examplePromptDiv.scrollIntoView({ behavior: "smooth", block: "start" }); // Scroll naar voorbeeldprompt
+        examplePromptDiv.scrollIntoView({ behavior: "smooth", block: "start" });
     };
 
     const resetForm = () => {
